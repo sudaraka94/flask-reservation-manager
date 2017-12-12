d93d31abff52ee486d102605fcbdd712a4fad0da
Sudaraka Jayathilaka <sudarakayasindu@gmail.com>
2017-12-12 12:19:34 +0530

    Reservation adding works

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ from flask import Flask, abort, request, jsonify, g, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_httpauth import HTTPBasicAuth
 from passlib.apps import custom_app_context as pwd_context
+from datetime import datetime
 
 
 '''
@@ -25,6 +26,18 @@ auth = HTTPBasicAuth()
 
 '''
 |-----------------------------------------------------------------
+| Number of tables in the restaurent
+|-----------------------------------------------------------------
+| Here we make an assumption that every table in the restaurant 
+| has equal number of chairs. By the term "one reservation"
+| a single table will be reserved. One can reserve the table per 
+| single day and only one reservation can be made for a single 
+| table per single day  
+'''
+number_of_tables=10
+
+'''
+|-----------------------------------------------------------------
 | User model
 |-----------------------------------------------------------------
 | Code responsible for the user model. This acts as the user   
@@ -36,7 +49,7 @@ auth = HTTPBasicAuth()
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), index=True)
+    telephone = db.Column(db.String(32), index=True)
     password_hash = db.Column(db.String(64))
 
     def hash_password(self, password):
@@ -47,14 +60,28 @@ class User(db.Model):
 
 '''
 |-----------------------------------------------------------------
+| Reservation model
+|-----------------------------------------------------------------
+| Code responsible for the reservation model. This acts as the    
+| reservation object model used for the ORM. 
+|-----------------------------------------------------------------
+'''
+class Reservation(db.Model):
+    __tablename__ = 'reservations'
+    id = db.Column(db.Integer, primary_key=True)
+    telephone = db.Column(db.String(32))
+    date = db.Column(db.DateTime, index=True)
+
+'''
+|-----------------------------------------------------------------
 | Password Authentication
 |-----------------------------------------------------------------
 '''
 
 @auth.verify_password
-def verify_password(username, password):
+def verify_password(telephone, password):
     # try to authenticate with username/password
-    user = User.query.filter_by(username=username).first()
+    user = User.query.filter_by(telephone=telephone).first()
     if not user or not user.verify_password(password):
             return False   
     g.user = user
@@ -68,17 +95,17 @@ def verify_password(username, password):
 
 @app.route('/api/users', methods=['POST'])
 def new_user():
-    username = request.json.get('username')
+    telephone = request.json.get('telephone')
     password = request.json.get('password')
-    if username is None or password is None:
+    if telephone is None or password is None:
         abort(400)    # missing arguments
-    if User.query.filter_by(username=username).first() is not None:
+    if User.query.filter_by(telephone=telephone).first() is not None:
         abort(400)    # existing user
-    user = User(username=username)
+    user = User(telephone=telephone)
     user.hash_password(password)
     db.session.add(user)
     db.session.commit()
-    return (jsonify({'username': user.username}), 201,
+    return (jsonify({'telephone': user.telephone}), 201,
             {'Location': url_for('get_user', id=user.id, _external=True)})
 
 
@@ -87,12 +114,32 @@ def get_user(id):
     user = User.query.get(id)
     if not user:
         abort(400)
-    return jsonify({'username': user.username})
+    return jsonify({'telephone': user.telephone})
 
 @app.route('/api/resource')
 @auth.login_required
 def get_resource():
-    return jsonify({'data': 'Hello, %s!' % g.user.username})
+    return jsonify({'data': 'Hello, %s!' % g.user.telephone})
+
+@app.route('/api/reservation',methods=['POST'])
+@auth.login_required
+def make_reservation():
+    date=request.json.get('date')
+    user_id=request.authorization.username
+    
+    #Processing date and creating python datetime object
+    date_obj=datetime.strptime(date, '%Y-%m-%d')
+    reservations= Reservation.query.filter_by(date=date_obj).count()
+    
+    if reservations<number_of_tables :
+        #More resevations can be made
+        reservation=Reservation(telephone=user_id,date=date_obj)
+        db.session.add(reservation)
+        db.session.commit()
+        return(jsonify({'status':'Reservation added successfully'}))
+
+    else :
+        return(jsonify({'status':'Reservation adding failed !. We cannot accomodate any more reservations for the mentioned date'}))
 
 
 '''
